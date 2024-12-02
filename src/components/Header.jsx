178f3b05e413ac400cad3d4f7624dd8e3768329e
Dilmi Siriwardhana <136610404+DilmiSiriwardhana@users.noreply.github.com>
2024-12-02 20:49:35 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -249,16 +249,7 @@ const Header = () => {
           <MenuItem onClick={handleClose}>
             <Avatar /> Profile
           </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Avatar /> My account
-          </MenuItem>
           <Divider />
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <PersonAdd fontSize="small" />
-            </ListItemIcon>
-            Add another account
-          </MenuItem>
           <MenuItem onClick={handleClose}>
             <ListItemIcon>
               <Settings fontSize="small" />
